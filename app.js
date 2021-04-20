c1fd32e4f64aa8962d46b7f29fce537591f2fe81
GMDorneles <gabrielly.maldonadoo@gmail.com>
2021-04-20 12:43:10 -0400

    final do curso

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //o express proteje  o acesso as pastas onde estão os index, permiindo apenas acessar o que etá nas sessions
-const express       = require('express');//cria servidor
-const app           = express();//para definir rotas e funcionalidades utilizadas pelo express
-const db            =require('./db/conection');
-const path          =require("path");
-const exphbs        = require ("express-handlebars");//conexao entre express e handle bars
+const express       =  require('express');//cria servidor
+const app           =  express();//para definir rotas e funcionalidades utilizadas pelo express
+const db            = require('./db/conection');
+const path          = require("path");
+const exphbs        =  require ("express-handlebars");//conexao entre express e handle bars
+const Job           = require('./models/Job');
+const Sequelize     = require('sequelize');
+const Op = Sequelize.Op;//para consultas mais complexas
 //pacote instalado para importar dados  do JOB.js
-const bodyParser    = require('body-parser');//permite interagir com dadosde formulario
+const bodyParser    =  require('body-parser');//permite interagir com dadosde formulario
 
-const PORT = 3002;//porta
+const PORT = 3003;//porta
 //faz serv ouvir
 app.listen(PORT, function(){
     console.log(`o express edta na porta ${PORT}`);
@@ -16,15 +19,16 @@ app.listen(PORT, function(){
 //app.use é para utilizar  a session
 //pega dados
 app.use(bodyParser.urlencoded({extended:false}));
-//handlebars
-//onde fica o diretorio com os dados a serem renderizados acessa a past  viewa
-app.set('views',path.join(__dirname,'views'));
-app.engine('handlebars',exphbs({defaultLayout:'main'}));//arquivo principal  a ser carregado
-app.set('view engine','handlebars');
-
 //Static folder qual a pasta de arquivos estáticos
 //ai nao precisa mais do public no href
 app.use(express.static(path.join(__dirname,'public')));
+//main.handlebars
+//onde fica o diretorio com os dados a serem renderizados acessa a past  viewa
+app.set('views',path.join(__dirname,'views'));
+app.engine('handlebars',exphbs({defaultLayout:'main'}));//arquivo principal  a ser carregado
+app.set('view engine','handlebars');//framework de renderização de views
+
+
 
 //db connection 
 db
@@ -43,10 +47,39 @@ db
 //pode ser usaddo um set time out
 //req requisição mensagens enviadas pelo client
 //res resposta mensagens enviadsa pelo serv
-app.get('/',(req,res)=> {//rot
-    //se quiser pegar dados de forulario: req.body.login (usando bdy parser)
-    res.render('index');//arquivo index.handlebars 
+
+app.get("/", (req, res) => {
+    let search = req.query.job;
+    let query = "%" + search + "%";
+//ve se tem parametro ou n
+    if (!search) {
+        Job.findAll({
+            order: [
+                ["createdAt", "DESC"]
+            ]
+        })
+            .then(jobs => {
+                res.render("index", {
+                    jobs
+                });
+            })
+            .catch(err => console.log(err));
+    } else {
+        Job.findAll({
+            where: { title: { [Op.like]: query } },
+            order: [
+                ["createdAt", "DESC"]
+            ]
+        })
+            .then(jobs => {
+                res.render("index", {
+                    jobs, search
+                });
+            })
+            .catch(err => console.log(err));
+    }
 });
+
 //rotas
 //inicio de todas as rotas
-app.use('/jobs', require('./routes/jobs'));
+app.use("/jobs", require("./routes/jobs"));
--- routes/jobs.js
+++ routes/jobs.js
@@ -5,7 +5,24 @@ const job       = require('../models/Job');
 
 router.get('/test',(req,res)=>{
     res.send('deu certo');
-}); 
+});
+//detalhe da vaga
+//pega por id
+router.get('/view/:id',(req,res)=>job.findOne({//busca 1 onde foi igual o id
+    where:{id: req.params.id} //compara
+}).then(job=>{//view 
+    res.render('view',{
+        job//dado do backend
+    });
+}).catch(err=>console.log(err)));
+
+
+router.get('/add',(req,res)=>{
+
+    res.render("add");
+
+})
+
 //inserção no sistema
 //add job via post
 router.post('/add',(req, res) =>{//variaveis vem do body que é importado no app
